1e6223e07b2a6f120c0a553b98fa84342618b4d8
AnnaConcs <concolatoanna@gmail.com>
2026-09-23 13:58:58 -0300

    ex1 terminado

---
+++ rec01_producao.js
@@ -0,0 +1,9 @@
+const CaixasPorHora = 75;
+const horasTrabalhadas = 8;
+
+const producaoTotal = CaixasPorHora * horasTrabalhadas;
+
+console.log("=== PRODUÇÃO DO TURNO ===");
+console.log(`Caixas por hora: ${CaixasPorHora} Caixas`);
+console.log(`Horas trabalhadas: ${horasTrabalhadas} horas`);
+console.log(`total produzido: ${producaoTotal} caixas`);
